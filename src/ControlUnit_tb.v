aedc731a127b030559484bc6b4b6c6a96634edf1
Robohrriday <robohrriday.8@gmail.com>
2024-11-12 15:43:09 +0530

    Major Optimization

--- src/ControlUnit_tb.v
+++ src/ControlUnit_tb.v
@@ -5,6 +5,7 @@ reg clk;
 reg top_en;
 reg infer;
 reg [9:0] infer_addr;
+wire [31:0] infer_data;
 wire ID;
 wire IF;
 wire REG;
@@ -14,6 +15,8 @@ wire WB;
 wire JU;
 wire BR;
 wire SK;
+wire [6:0] LED_out;
+wire [3:0] Anode_Activate;
 
 initial begin
     clk <= 1;
@@ -25,6 +28,7 @@ ControlUnit uut(
 .top_en(top_en),
 .infer(infer),
 .infer_addr(infer_addr),
+.infer_data(infer_data),
 .ID(ID),
 .IF(IF),
 .REG(REG),
@@ -33,7 +37,9 @@ ControlUnit uut(
 .WB(WB),
 .JU(JU),
 .BR(BR),
-.SK(SK)
+.SK(SK),
+.LED_out(LED_out),
+.Anode_Activate(Anode_Activate)
 );
 
 initial begin
@@ -41,10 +47,12 @@ initial begin
     infer <= 0;
     #5;
     top_en <= 1;
-    #990;
+    #390;
     infer <= 1;
-    infer_addr <= 1;
-    #10;
+    infer_addr <= 2;
+    #20;
+    infer_addr <= 3;
+    #30;
     $finish();
 end
  
